05d11ccd90c5681b35ad9be1a10501e647afa243
unknown <2705056589@qq.com>
2025-12-28 15:29:15 +0800

    ai初步

---
+++ yudao-gateway/src/test/java/com/example/yudaogateway/YudaoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yudaogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YudaoGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
